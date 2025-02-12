ac8c507cf2a4edb009afb9a6bd224873b2b1827c
LautaroCavichia <lautarocavichia@gmail.com>
2025-02-12 17:52:15 +0100

    added test for multiple .sst files

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -18,3 +18,48 @@ TEST(DBTest, RecoveryAfterCrash) {
     }
 }
 
+TEST(DBTest, MultiFlushAndReload) {
+    const string data_dir =  "./test_data_multi_flush";
+    const size_t memtable_size = 100;
+    int num_entries = 0;
+
+    filesystem::remove_all(data_dir);
+
+    // Insert enough data to trigger 2 flushes
+    {
+        DB db (memtable_size, data_dir);
+
+        
+        while (num_entries < 30) {
+            string key = "key" + to_string(num_entries);
+            string value = "value" + to_string(num_entries);
+            db.insert(key,value);
+            num_entries++;
+        }
+
+        int sst_count = 0;
+        for (const auto& entry : filesystem::directory_iterator(data_dir)) {
+            if (entry.path().extension() == ".sst")
+                sst_count++;
+        }
+
+        ASSERT_GE(sst_count, 2) << "Expected at least 2 tables";
+    }
+
+    // reload DB and verify data exists 
+    {
+        DB db(memtable_size, data_dir);
+
+        for (int i = 0; i < num_entries; i++) {
+            string key = "key" + to_string(i);
+            string expected_value = "value" + to_string(i);
+            string actual_value = db.get(key);
+
+            ASSERT_EQ(actual_value, expected_value) << " Mismatch for key = " << key;
+
+        }
+    }
+
+    filesystem::remove_all(data_dir);
+}
+
